2b15de58c93b1d4b16f8b69ecb17a8f5a01aa289
chao-mu <chao.mu@minorcrash.com>
2012-02-25 10:16:46 -0500

    Add a constant for presets and general cleanup

--- config/setup.rb
+++ config/setup.rb
@@ -1,6 +1,12 @@
 PROJECT_BASE_DIR = File.join(File.dirname(__FILE__), '..')
-MODULE_DIR = File.join(PROJECT_BASE_DIR, 'modules')
-LIB_DIR = File.join(PROJECT_BASE_DIR, 'lib')
-CONFIG_DIR = File.join(PROJECT_BASE_DIR, 'config')
+
+def join_with_base(path)
+  File.join(PROJECT_BASE_DIR, path)
+end
+
+MODULE_DIR  = join_with_base('modules')
+LIB_DIR     = join_with_base('lib')
+CONFIG_DIR  = join_with_base('config')
+PRESETS_DIR = join_with_base('presets')
 
 $:.unshift(LIB_DIR)
